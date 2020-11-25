35f279acada7f221d2fcc3d185dcbd4082ff8866
whatTHEh3ll <yeah@tutanota.de>
2020-11-25 15:46:48 -0800

    11-25-2020

--- roles/recon/tasks/findomain-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/$USER/findomain \
-
-&& cargo build --release \
-
-&& sudo cp target/release/findomain /usr/bin/
-
-           
--- roles/recon/tasks/go-tools.sh
+++ roles/recon/tasks/go-tools.sh
@@ -6,7 +6,7 @@ echo "Installing go tools"
 echo "installed gobuster"
 /home/$USER/.go/bin/go get -u github.com/mhmdiaa/waybackunifier 
 echo "installed waybackunifiier"
-/home/$USER/.go/bin/go get -u github.com/tomnomnom/httprobe 
+#/home/$USER/.go/bin/go get -u github.com/tomnomnom/httprobe #
 echo "Installed httprobe"
 /home/$USER/.go/bin/go get github.com/haccer/subjack
 echo "Installed subjack"
@@ -18,18 +18,18 @@ GO111MODULE=on /home/$USER/.go/bin/go get -u -v github.com/lc/gau
 echo "Installed Get all urls"
 /home/$USER/.go/bin/go get -u github.com/tomnomnom/qsreplace
 echo "Installed qsreplace"
-/home/$USER/.go/bin/go get -u github.com/j3ssie/metabigor
+#/home/$USER/.go/bin/go get -u github.com/j3ssie/metabigor #
 echo "Installed metabigor"
 /home/$USER/.go/bin/go get github.com/hakluke/hakrawler
 echo "Installed hakrawler"
 GO111MODULE=on /home/$USER/.go/bin/go get -u -v github.com/projectdiscovery/shuffledns/cmd/shuffledns
 echo "Installed shuffledns"
-/home/$USER/.go/bin/go get -u github.com/tomnomnom/assetfinder
+#/home/$USER/.go/bin/go get -u github.com/tomnomnom/assetfinder #
 echo "Installed Assetfinder"
 /home/$USER/.go/bin/go get -u github.com/ffuf/ffuf
 echo "Installed ffuf"
-GO111MODULE=auto /home/$USER/.go/bin/go -u -v github.com/projectdiscovery/httpx/cmd/httpx
+GO111MODULE=auto /home/$USER/.go/bin/go get -u -v github.com/projectdiscovery/httpx/cmd/httpx
 echo "Installed httpx"
-GO111MODULE=on /home/$USER/.go get -u -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+GO111MODULE=on /home/$USER/.go/bin/go get -u -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
 echo "Installed Nuclei"
 echo "done installing go tools"
