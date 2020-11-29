fcadbb046708345a73506ddb1537a63e5a72e273
whatTHEh3ll <yeah@tutanota.de>
2020-11-28 16:13:08 -0800

    move towards with_items in playbook and made the README pretty

--- roles/recon/tasks/go-tools.sh
+++ roles/recon/tasks/go-tools.sh
@@ -6,7 +6,7 @@ echo "Installing go tools"
 echo "installed gobuster"
 /home/$USER/.go/bin/go get -u github.com/mhmdiaa/waybackunifier 
 echo "installed waybackunifiier"
-#/home/$USER/.go/bin/go get -u github.com/tomnomnom/httprobe #
+/home/$USER/.go/bin/go get -u github.com/tomnomnom/httprobe #
 echo "Installed httprobe"
 /home/$USER/.go/bin/go get github.com/haccer/subjack
 echo "Installed subjack"
@@ -18,13 +18,13 @@ GO111MODULE=on /home/$USER/.go/bin/go get -u -v github.com/lc/gau
 echo "Installed Get all urls"
 /home/$USER/.go/bin/go get -u github.com/tomnomnom/qsreplace
 echo "Installed qsreplace"
-#/home/$USER/.go/bin/go get -u github.com/j3ssie/metabigor #
+/home/$USER/.go/bin/go get -u github.com/j3ssie/metabigor 
 echo "Installed metabigor"
 /home/$USER/.go/bin/go get github.com/hakluke/hakrawler
 echo "Installed hakrawler"
 GO111MODULE=on /home/$USER/.go/bin/go get -u -v github.com/projectdiscovery/shuffledns/cmd/shuffledns
 echo "Installed shuffledns"
-#/home/$USER/.go/bin/go get -u github.com/tomnomnom/assetfinder #
+/home/$USER/.go/bin/go get -u github.com/tomnomnom/assetfinder 
 echo "Installed Assetfinder"
 /home/$USER/.go/bin/go get -u github.com/ffuf/ffuf
 echo "Installed ffuf"
